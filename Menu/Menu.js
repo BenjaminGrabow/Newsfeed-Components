1b65dd682bf73aa95801bae9e16080874d23e33f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-16 09:55:18 +0200

    Revert "WEBEU2 - Benjamin Grabow - Anthony Campbell - Week 4 Day 3"

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,10 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle("menu--open");
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+const menu;
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
+const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
